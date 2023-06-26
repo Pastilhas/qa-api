3a7be4713833e261a52a2256c0145791c561b381
Pastilhas <joao.campos1221@gmail.com>
2023-06-26 15:41:16 +0100

    Add device_map

--- src/app/engine.py
+++ src/app/engine.py
@@ -23,7 +23,9 @@ class Engine:
             generate_kwargs={"temperature": 0.7, "do_sample": False},
             system_prompt=system_prompt,
             query_wrapper_prompt=query_wrapper_prompt,
-            model_kwargs={"load_in_4bit": True},
+            device_map="sequential",
+            model_kwargs={"device_map":"sequential","load_in_4bit": True},
+            tokenizer_kwargs={"device_map":"sequential"},
         )
         service_context = ServiceContext.from_defaults(
             chunk_size=1024, llm_predictor=stablelm_predictor
